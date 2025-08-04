daea31a313084392390db78b03a0f0693d705eca
Vitalii Volosenko <vitalii.volosenko@ukr.net>
2025-08-04 15:19:14 +0300

    add FEN::ApplyPosition

--- ChessLogic/src/utils/fen.h
+++ ChessLogic/src/utils/fen.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include "chess_logic_API.h"
+
+#include "core/game.h"
+
+namespace Chess {
+namespace FEN {
+    CHESSLOGIC_API void applyPosition(Game& game, const std::string& fen);
+}
+}
